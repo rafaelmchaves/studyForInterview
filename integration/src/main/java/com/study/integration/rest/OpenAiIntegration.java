a63687f6f5caf56dfceba11a617159bc5bf6a42d
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2025-03-11 07:52:41 -0300

    Remove unused parameter

--- integration/src/main/java/com/study/integration/rest/OpenAiIntegration.java
+++ integration/src/main/java/com/study/integration/rest/OpenAiIntegration.java
@@ -1,6 +1,5 @@
 package com.study.integration.rest;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
